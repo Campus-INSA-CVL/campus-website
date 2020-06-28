9f64f0ac0e52cd22e2f5bfd268f812cf843cc24b
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-06-28 19:38:23 +0200

    create a previewCard to show data

---
+++ components/PreviewCard.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  v-hover(v-slot:default="{ hover }")
+    v-card(:to="content.path", :elevation="hover ? 4 : 0")
+      v-card-title {{ content.title }}
+      v-card-text {{ content.description }}
+</template>
+
+<script>
+export default {
+  name: 'PreviewCard',
+  props: {
+    content: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
