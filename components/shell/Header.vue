6688ca16df4960337cbfca4c905eea511e667751
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-06-29 01:47:38 +0200

    fix header on small screen

--- components/shell/Header.vue
+++ components/shell/Header.vue
@@ -2,17 +2,26 @@
   v-app-bar(app, elevate-on-scroll)
     v-app-bar-nav-icon(@click.stop="toggleDrawer()")
     template(v-slot:extension, v-if="type === 'association'")
-      v-btn(icon, nuxt, to='/federation', exact, :small="isSmAndDown").text--secondary
+      v-btn(icon, nuxt, to='/federation', exact).text--secondary
         v-icon mdi-web
       v-btn(:to="fullPath", depressed, nuxt, :color="`${color}--text`").pole-btn
         v-toolbar-title.font-weight-bold.text-capitalize {{ title }}
       v-spacer
-      v-btn(outlined, :color="`${color}`", :to="`${fullPath}/equipe`", nuxt, :small="isSmAndDown").mr-4
+      v-btn(outlined, :color="`${color}`", :to="`${fullPath}/equipe`", nuxt, v-if="!isSmAndDown").mr-4
         v-icon(:left="!isSmAndDown") mdi-account-group
         span(v-if="!isSmAndDown") l'équipe
-      v-btn(depressed, :color="`${color}`", :to="`${fullPath}/associations`", nuxt, dark, :small="isSmAndDown")
-        v-icon(:left="!isSmAndDown") mdi-file-tree
-        span(v-if="!isSmAndDown") les associations
+      v-btn(depressed, :color="`${color}`", :to="`${fullPath}/associations`", nuxt, dark, v-if="!isSmAndDown")
+        v-icon(left) mdi-file-tree
+        span les associations
+      v-menu(offset-y, v-if="isSmAndDown")
+        template(v-slot:activator="{ on, attrs }")
+          v-btn(depressed, :color="`${color}`", dark, v-bind="attrs", v-on="on")
+            v-icon {{ JSON.parse(attrs['aria-expanded']) ? 'mdi-menu-up' : 'mdi-menu-down'}}
+        v-list(:color="`${color}--text`", flat).text-uppercase
+          v-list-item(nuxt, :to="`${fullPath}/equipe`")
+            v-list-item-title l'équipe
+          v-list-item(nuxt, :to="`${fullPath}/associations`")
+            v-list-item-title les associations
 </template>
 
 <script>
