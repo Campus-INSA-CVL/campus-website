de03b5ad1521bb8ea3e8b5f1bb29de09de9a1cd8
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-06-28 19:36:28 +0200

    create olympiades route

---
+++ components/Carousel.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  v-carousel(cycle, continous, show-arrows-on-hover, interval="3000", height="100%")
+    v-carousel-item(v-for="(name, index) in names", :key="index")
+      v-img(:src="requireLink(name)", aspect-ratio="1")
+</template>
+
+<script>
+export default {
+  props: {
+    names: {
+      type: Array,
+      default: () => [],
+      required: true,
+    },
+    folderName: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    requireLink(link) {
+      return require(`../assets/${this.folderName}/${link}`)
+    },
+  },
+}
+</script>
---
+++ pages/olympiades/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  nuxt-content(:document="page")
+</template>
+
+<script>
+import Carousel from '@/components/Carousel'
+
+export default {
+  components: {
+    Carousel,
+  },
+  async asyncData({ $content }) {
+    const page = await $content('olympiades/index').fetch()
+
+    return {
+      page,
+    }
+  },
+}
+</script>
