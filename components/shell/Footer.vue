481017b82f390c41bc23314a5816963c7155816c
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-07-01 20:45:37 +0200

    add link to footer

--- components/shell/Footer.vue
+++ components/shell/Footer.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
   v-footer(:app="$vuetify.breakpoint.lgAndUp")
     v-row(align="center", justify="center", no-gutters)
-      v-col(cols="12", sm="6", :order="$vuetify.breakpoint.smAndDown ? '1' : '0'")
+      v-col(cols="12", sm="8", :order="$vuetify.breakpoint.xs ? '1' : '0'")
         div.text-center.font-weight-light.text-secondary.mx-2 {{ new Date().getFullYear() }} - Campus INSA Centre-Val de Loire
-      v-col(cols="12", sm="6", align="center", v-if="socialNetworks")
-        v-btn(v-for="icon in icons", :key="icon", icon).text--secondary.mx-2
-          v-icon {{ icon }}
+      v-col(cols="12", sm="4", align="center", v-if="socialNetworks")
+        v-btn(v-for="network in networks", :key="network.path", icon).text--secondary.mx-2
+          v-icon {{ network.icon }}
 </template>
 
 <script>
@@ -18,7 +18,16 @@ export default {
   },
   data() {
     return {
-      icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
+      networks: [
+        {
+          icon: 'mdi-facebook',
+          path: 'https://www.facebook.com/campusinsacvl',
+        },
+        {
+          icon: 'mdi-instagram',
+          path: 'https://www.instagram.com/campusinsacvl/',
+        },
+      ],
     }
   },
 }
