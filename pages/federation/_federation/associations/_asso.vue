e147110c248f6c5767c7625ec0b81346c34bb944
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-07-02 14:41:16 +0200

    fix issue

--- pages/federation/_federation/associations/_asso.vue
+++ pages/federation/_federation/associations/_asso.vue
@@ -1,25 +1,37 @@
 <template lang="pug">
-  nuxt-content(:document="content", :class="color")
+  nuxt-content(:document="content", :id="color")
 </template>
 
 <script>
-import fetchContent from '@/mixins/fetch-content'
+// import fetchContent from '@/mixins/fetch-content'
 
 export default {
-  mixins: [
-    fetchContent({
-      folderName: 'federation',
-      paramsNames: ['federation', 'asso'],
-      fileName: 'index',
-    }),
-  ],
-  layout({ params }) {
-    return params.federation ?? 'default'
+  async asyncData({ $content, params }) {
+    const { federation, asso } = params
+    // let content = $content('federation', federation, asso, 'index')
+    let content = $content(`federation/${federation}/${asso}/index`)
+    content = await content.fetch()
+    return {
+      content,
+    }
   },
+  // mixins: [
+  //   fetchContent({
+  //     folderName: 'federation/culturel/insarpege',
+  //     paramsNames: [],
+  //     fileName: 'index',
+  //   }),
+  // ],
   computed: {
     color() {
       return this.$route.params.federation ?? ''
     },
+  },
+  created() {
+    console.log(' this.$route:', this.$route)
+  },
+  layout({ params }) {
+    return params.federation ?? 'default'
   },
 }
 </script>
