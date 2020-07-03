c92df1e46837b70be1005ebb93ffa61b12b1a9bc
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-07-03 18:12:12 +0200

    adding support of fallback in prod mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,9 @@ export default {
    ** See https://nuxtjs.org/api/configuration-target
    */
   target: 'server',
+  generate: {
+    fallback: true,
+  },
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
