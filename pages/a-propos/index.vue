60a392712e32df4377d4e7630decb2169f9355a5
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-07-08 20:41:43 +0200

    add the version

--- pages/a-propos/index.vue
+++ pages/a-propos/index.vue
@@ -5,8 +5,11 @@
         v-col(cols="12", align="center")
           v-img(:src="campusPath", max-width="100")
         v-col(cols="12", sm="8", align="center")
-          p.font-weight-medium Ce site a été conçu par des élèves-ingénieurs de l'INSA Centre-Val de Loire. Pour tous problèmes ou retours, n'hésitez pas à nous contacter !<br />
+          p.font-weight-medium.mb-0 Ce site a été conçu par des élèves-ingénieurs de l'INSA Centre-Val de Loire. Pour tous problèmes ou retours, n'hésitez pas à nous contacter !<br />
             | Vous pouvez aussi retrouver l'ensemble de son code source sur Github !
+      v-row(justify="center", no-gutters, align="center")
+        v-col(cols="12")
+          div.text-secondary.font-weight-light.text-center.body-2 v0.8.0
       v-row(tag="section", justify="space-around")
         v-col(cols="12", md="4", align="center")
           v-btn(to="/nous-contacter", outlined, nuxt).primary--text
