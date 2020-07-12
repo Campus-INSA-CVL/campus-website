bfec0dfb15f9eab253d7e1bea3e815d93501452f
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-07-12 21:02:13 +0200

    add animation on intersection

--- components/global/PreviewCard.vue
+++ components/global/PreviewCard.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   v-hover(v-slot:default="{ hover }")
-    v-card(:to="path", nuxt, :elevation="hover ? 4 : 0", )
+    v-card(:to="path", nuxt, :elevation="hover ? 4 : 0", v-intersect.once="{ handler: onIntersect, options: { threshold: 0.4 } }")
       v-card-title(:class="`${content.color}--text`").font-weight-bold.text-uppercase {{ content.title }}
       v-card-text {{ content.description }}
       v-card-actions
@@ -20,6 +20,23 @@ export default {
   computed: {
     path() {
       return this.content.path
+    },
+  },
+  mounted() {
+    const cards = document.getElementsByClassName('v-card')
+    for (let index = 0; index < cards.length; index++) {
+      const card = cards[index]
+      card.classList.add('invisible')
+    }
+  },
+  methods: {
+    onIntersect(entries, observer, isIntersecting) {
+      if (isIntersecting) {
+        entries[0].target.classList.add('moveup')
+        entries[0].target.classList.remove('invisible')
+      } else {
+        entries[0].target.classList.remove('moveup')
+      }
     },
   },
 }
