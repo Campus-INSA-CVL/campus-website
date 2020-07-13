0416b36d6623daba46fece692aad678dcb70e17b
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-07-13 11:05:24 +0200

    fix size issue

--- components/global/Center.vue
+++ components/global/Center.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
   v-row(justify="center")
-    v-col(cols="12", md="10", lg="8", xl="6", align="center")
+    v-col(cols="12", md="8", lg="6", xl="4", align="center")
       slot
 </template>
