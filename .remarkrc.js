93db11dd1d78707b9dda4b64ac2aa90968099763
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-08-11 01:14:23 +0200

    lint all md files

--- .remarkrc.js
+++
@@ -1,8 +0,0 @@
-exports.plugins = [
-  'remark-preset-lint-recommended',
-  'remark-preset-lint-markdown-style-guide',
-  'remark-frontmatter',
-  ['remark-lint-list-item-indent', 'space'],
-  ['remark-lint-emphasis-marker', '_'],
-  ['remark-lint-list-item-spacing', false],
-]
