a144dc49e62cacf22972f061b06c3e3d35afd551
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-08-21 13:58:36 +0200

    fix name in a-propos

--- pages/a-propos/index.vue
+++ pages/a-propos/index.vue
@@ -9,7 +9,7 @@
             | Vous pouvez aussi retrouver l'ensemble de son code source sur Github !
         v-col(cols="12", sm="8", align="center")
           div.text-capitalize.font-weight-light.primary--text développeur: #[span.font-weight-bold SOUBIRAN Estéban]
-          div.text-capitalize.font-weight-light.primary--text contributeur: #[span.font-weight-bold BEZIAUD Jordan] #[span.font-weight-bold RICHARD Jeremy]
+          div.text-capitalize.font-weight-light.primary--text contributeurs: #[span.font-weight-bold BEZIAUD Jordan] / #[span.font-weight-bold RICHARD Jérémy]
       v-row(justify="center", no-gutters, align="center")
         v-col(cols="12")
           div.text-secondary.font-weight-light.text-center.body-2 v0.8.0
