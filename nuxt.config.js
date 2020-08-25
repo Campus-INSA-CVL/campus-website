5c4eca3f0b88d36f559c6eb1eacec93ffa9aaf5b
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-08-25 22:31:22 +0200

    update for heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,12 +8,12 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  mode: 'spa',
+  mode: 'universal',
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  target: 'server',
   generate: {
     fallback: true,
   },
