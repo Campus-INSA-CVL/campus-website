a73fdbc0c5dc749838dd85cfa0fdaa0ffae8e61b
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-09-12 11:23:50 +0200

    fix(pages): remove the size limitation in ve

--- pages/vie-etudiante/_tab.vue
+++ pages/vie-etudiante/_tab.vue
@@ -1,7 +1,5 @@
 <template lang="pug">
-  v-row(tag="section", no-gutters)
-    v-col(cols="12", md="10", lg="8", offset-md="1", offset-lg="2")
-      nuxt-content(:document="page", class="prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto")
+  nuxt-content(:document="page", class="prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto")
 </template>
 
 <script>
