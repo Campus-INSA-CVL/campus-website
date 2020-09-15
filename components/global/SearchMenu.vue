af3c7cbd560048e82613c58dd2e4a1991e3734a8
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-09-15 02:43:43 +0200

    feat(components): second draft of search bar

---
+++ components/global/SearchMenu.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  v-menu(offset-y)
+    template(v-slot:activator="{ on, attrs }")
+      search(:on="on", v-bind="attrs", flat, solo, dense, clearable, label="Chercher", hide-details, rounded, @search="searchData")
+    v-list(dense, two-line, v-if="searchResults.length !== 0")
+      v-list-item-group(color="primary")
+        v-subheader Résultats
+        v-list-item(v-for="(item, index) in searchResults", :key="index", nuxt, :to="item.path", @click="clearSearchData()")
+          v-list-item-content
+            v-list-item-title {{ item.title }}
+            v-list-item-subtitle {{ item.description }}
+</template>
+
+<script>
+export default {
+  name: 'SearchMenu',
+  data() {
+    return {
+      searchResults: [],
+    }
+  },
+  methods: {
+    clearSearchData() {
+      this.searchResults = []
+    },
+    searchData(data) {
+      this.searchResults = data
+    },
+  },
+}
+</script>
