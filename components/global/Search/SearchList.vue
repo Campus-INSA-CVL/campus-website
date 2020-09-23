54935539ec980f2b9aa409ee4bca154e3b212b1a
Esteban S <esteban.soubiran@insa-cvl.fr>
2020-09-23 17:59:10 +0200

    fix(search): fix path design issue

--- components/global/Search/SearchList.vue
+++ components/global/Search/SearchList.vue
@@ -2,7 +2,7 @@
   v-list(dense, two-line, max-width="400")
     v-list-item-group(color="primary")
       v-subheader {{ message }}
-      v-list-item(v-for="(item, index) in value", :key="index", nuxt, :to="item.path", @click="clearSearchData()")
+      v-list-item(v-for="(item, index) in value", :key="index", nuxt, :to="item.path", @click="clearSearchData()", exact)
         v-list-item-content
           v-list-item-title.text-uppercase {{ item.title }}
           v-list-item-subtitle {{ item.description }}
