3d88f81d73f8228d6345fe4c129b211eb4a7c049
DragonKEME <kiki.lassa@outlook.com>
2023-08-25 21:38:19 -0400

    Add: website icon in social component

--- components/campus/Social.vue
+++ components/campus/Social.vue
@@ -20,6 +20,7 @@ import {
   mdiDiscord,
   mdiLinkedin,
   mdiYoutube,
+  mdiLink,
 } from '@mdi/js'
 
 export default {
@@ -42,6 +43,7 @@ export default {
         discord: mdiDiscord,
         linkedin: mdiLinkedin,
         youtube: mdiYoutube,
+        website: mdiLink,
       },
     }
   },
